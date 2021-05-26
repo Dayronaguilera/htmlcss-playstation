c0e7b3e9494eb0f34faa6d14af45d9566b67a8d2
Dayronaguilera <aguilera.dayron@gmail.com>
2021-05-26 15:42:39 +0200

    End esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log (ciaone mondo);
+// questo e visibile solo dal programmatore sulla console
+console.log ('Console Playstation 5');
+
+//  questo e visibile dal utente 
+document.getElementById ('ps5-title').innerHTML='Console Playstation 5';
+
+// interazione con l'utente
+alert('stai per entrare in una pagina fantastica');
