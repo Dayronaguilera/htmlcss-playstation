76dea26a23d431d9d8acb48f41d3c2b43a715732
Dayronaguilera <aguilera.dayron@gmail.com>
2021-05-26 17:11:45 +0200

    End esercizio

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 console.log ('Console Playstation 5');
 
 //  questo e visibile dal utente (selezione tramite id)
-document.getElementById ('ps5-title').innerHTML='Console Playstation 5';
+document.getElementById ('ps5-title').innerHTML='Ecco la PlayStation 5';
 
-// questo e visibile dal utente (selezione tramite class)
-document.getElementsByClassName ('test-js').innerHTML='Sperimenta la nuova generazione di incredibili giochi PlayStation';
+// questo e visibile dal utente (selezione tramite class ps:va indicato il numero della classe nel caso ce ne siano di piu )
+document.getElementsByClassName ('test-js')[0].innerHTML='Sperimenta la nuova generazione di incredibili giochi PlayStation';
+
 
 // interazione con l'utente
-alert('stai per entrare in una pagina fantastica');
+alert('Stai per entrare in una pagina fantastica!');
+
+// prova bottone sul title
+function myFunction() {
+    var x = document.getElementsByClassName('test-button');
+    x[0].innerHTML='Console PlayStation 5';
+}
