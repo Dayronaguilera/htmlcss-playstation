ceae5d1cea62be78f7a2f45fe25802dfca67a35b
Dayronaguilera <aguilera.dayron@gmail.com>
2021-05-26 16:08:18 +0200

    Test con class name js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // questo e visibile solo dal programmatore sulla console
 console.log ('Console Playstation 5');
 
-//  questo e visibile dal utente 
+//  questo e visibile dal utente (selezione tramite id)
 document.getElementById ('ps5-title').innerHTML='Console Playstation 5';
+
+// questo e visibile dal utente (selezione tramite class)
+document.getElementsByClassName ('test-js').innerHTML='Sperimenta la nuova generazione di incredibili giochi PlayStation';
 
 // interazione con l'utente
 alert('stai per entrare in una pagina fantastica');
